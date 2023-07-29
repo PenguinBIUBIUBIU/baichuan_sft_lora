2b303f68d836d9343e1448f6125581d5a621187b
zhoulei <zhoulei_helloworld@163.com>
2023-07-30 00:33:35 +0800

    一次运行多个对话, 对话写入文件

--- infer.py
+++ infer.py
@@ -20,10 +20,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--model_name_or_path", type=str, help="model_name_or_path")
 parser.add_argument("--lora_dir", type=str, help="lora_dir")
 parser.add_argument("--prompts_path", type=str, help="prompts_path")
+parser.add_argument("--output_path", type=str, help="output_path")
 args = parser.parse_args()
 MODEL_NAME_OR_PATH = args.model_name_or_path
 LORA_DIR = args.lora_dir
 PROMPTS_PATH = args.prompts_path
+OUTPUT_PATH = args.output_path
 ###加载量化模型
 device_map = {"": 0}
 print(f"{MODEL_NAME_OR_PATH}:加载模型")
@@ -70,15 +72,20 @@ if not prompts:
     prompts.append("""北京有啥好玩的地方""")
 
 
-def chat(promts: List[str]):
-    for prompt in promts:
-        print(f"prompt:{prompt}")
-        inputttext = """###Human:\n{}###Assistant:\n:
-        """.format(prompt)
-        inputs = tokenizer(prompt, return_tensors="pt").to(device)
-        generate_ids = model_lora.generate(**inputs, generation_config=generation_config)
-        output = tokenizer.decode(generate_ids[0])
-        print(output)
+def chat(promts: List[str], output_file_name: str):
+    with open(output_file_name, "w") as output_file:
+        for prompt in promts:
+            print(f"prompt: {prompt}")
+            input_text = """###Human:\n{}###Assistant:\n""".format(prompt)
+            inputs = tokenizer(prompt, return_tensors="pt").to(device)
+            generate_ids = model_lora.generate(**inputs, generation_config=generation_config)
+            output = tokenizer.decode(generate_ids[0])
+            print(f"response: {output}")
+
+            # 将prompt和response同时写入文件
+            output_file.write(f"prompt: {prompt}\n")
+            output_file.write(f"response: {output}\n\n")
+
 
 
 chat(prompts)
